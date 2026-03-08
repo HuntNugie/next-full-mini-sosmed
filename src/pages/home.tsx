a673f33c4ed00397992379a912e12f76ccc8b3c1
nugie kurniawan <nugiebinhar@gmail.com>
2026-03-08 20:00:38 +0700

    menambahkan gate pengecekan

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,8 @@
 import MainLayout from "@/components/layouts/main.layout";
 import HomeView from "@/components/views/main/home.view";
+import { GetServerSidePropsContext } from "next";
+import { getServerSession } from "next-auth";
+import { AuthOptions } from "./api/auth/[...nextauth]";
 
 export default function HomePage() {
     return (<>
@@ -7,4 +10,21 @@ export default function HomePage() {
             <HomeView />
         </MainLayout>
     </>)
+}
+
+export async function getServerSideProps(ctx:GetServerSidePropsContext){
+    const session = await getServerSession(ctx.req,ctx.res,AuthOptions)
+    if(!session){
+        return {
+            redirect:{
+                destination:"/login",
+                permanent:false
+            }
+        }
+    }
+    return {
+        props:{
+            user:session.user
+        }
+    }
 }
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,8 @@
 import AuthLayout from "@/components/layouts/auth.layout";
 import LoginView from "@/components/views/auth/login.view";
+import { GetServerSidePropsContext } from "next";
+import { getServerSession } from "next-auth";
+import { AuthOptions } from "./api/auth/[...nextauth]";
 
 export default function LoginPage() {
     return (<>
@@ -7,4 +10,18 @@ export default function LoginPage() {
             <LoginView />
         </AuthLayout>
     </>)
+}
+export async function getServerSideProps(ctx: GetServerSidePropsContext) {
+    const session = await getServerSession(ctx.req, ctx.res, AuthOptions);
+    if (session) {
+        return {
+            redirect: {
+                destination: "/home",
+                permanent: false
+            }
+        }
+    }
+    return {
+        props: {}
+    }
 }
